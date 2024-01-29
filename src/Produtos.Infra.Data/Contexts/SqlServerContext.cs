0b67a642281d477ea55b48a528b82db91d74b6b0
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2024-01-29 16:14:20 -0300

    migration adicionado

---
+++ src/Produtos.Infra.Data/Contexts/SqlServerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Produtos.Domain.Models;
+using Produtos.Infra.Data.Mappings;
+
+namespace Produtos.Infra.Data.Contexts
+{
+    /// <summary>
+    /// Classe para acesso ao banco de dados com o Entity Framework Core
+    /// </summary>
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //classe de mapeamento
+            modelBuilder.ApplyConfiguration(new ProdutoMap());
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ src/Produtos.Infra.Data/Contexts/SqlServerMigration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Produtos.Infra.Data.Contexts
+{
+    public class SqlServerMigration : IDesignTimeDbContextFactory<SqlServerContext>
+    {
+        public SqlServerContext CreateDbContext(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            
+            configurationBuilder.AddJsonFile(path, false);
+            
+            var root = configurationBuilder.Build();
+            var connectionString = root.GetSection("ConnectionStrings").GetSection("ApiProdutos").Value;
+            
+            var builder = new DbContextOptionsBuilder<SqlServerContext>();
+            builder.UseSqlServer(connectionString);
+            
+            return new SqlServerContext(builder.Options);
+        }
+    }
+}
