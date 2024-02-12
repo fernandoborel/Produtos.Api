0db0c0c107825309d9c1de6c713f29e3d928f441
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2024-02-12 14:00:35 -0300

    tests. post e put funcionando

---
+++ src/Produtos.Application/Commands/GetProdutoCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Produtos.Application.Commands
+{
+    public class GetProdutoCommand
+    {
+        public Guid IdProduto { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Total { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataUltimaAlteracao { get; set; }
+    }
+}
