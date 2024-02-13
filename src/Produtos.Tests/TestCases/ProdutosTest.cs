9ed4185637f75b94386f8d5db2679eae43056021
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2024-02-13 12:35:45 -0300

    tests. testes validados

--- src/Produtos.Tests/TestCases/ProdutosTest.cs
+++ src/Produtos.Tests/TestCases/ProdutosTest.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Newtonsoft.Json;
 using Produtos.Application.Commands;
+using Produtos.Domain.Models;
 using Produtos.Tests.Helpers;
 using System.Net;
 using System.Net.Http;
@@ -26,7 +27,7 @@ namespace Produtos.Tests.TestCases
 
 
         [Fact]
-        public async Task Post_Produtos_Return_Ok()
+        public async Task <GetProdutoCommand>Post_Produtos_Return_Ok()
         {
             #region Criando os dados da requisição
 
@@ -41,15 +42,28 @@ namespace Produtos.Tests.TestCases
 
             #region Executando os serviços da API
 
-            var result = await _client.PostAsync($"{ApiHelper.Endpoint}/produtos/cadastrar", ApiHelper.CreateContent(request));
-            
+            var result = await _client.PostAsync
+                ($"{ApiHelper.Endpoint}/produtos/cadastrar", ApiHelper.CreateContent(request));
+
+            var response = ApiHelper.CreateResponse<GetProdutoCommand>(result);
+
             #endregion
 
             #region Validando os resultados
 
-            result.StatusCode.Should().Be(HttpStatusCode.Created);
+            result
+                .StatusCode
+                .Should()
+                .Be(HttpStatusCode.Created);
+
+            response.IdProduto.Should();
+            response.Nome.Should();
+            response.Preco.Should();
+            response.Quantidade.Should();
 
             #endregion
+
+            return response;
         }
 
         [Fact]
@@ -57,76 +71,181 @@ namespace Produtos.Tests.TestCases
         {
             #region Cadastrando um Produto
 
-            var createRequest = new CriarProdutoCommand
-            {
-                Nome = _faker.Commerce.ProductName(),
-                Preco = decimal.Parse(_faker.Commerce.Price()),
-                Quantidade = 10
-            };
+            var request = await Post_Produtos_Return_Ok();
 
-            var createResult = await _client.PostAsync($"{ApiHelper.Endpoint}/produtos/cadastrar", ApiHelper.CreateContent(createRequest));
-            var createdProduct = ApiHelper.CreateResponse<GetProdutoCommand>(createResult);
+            request.Nome = _faker.Commerce.ProductName();
+            request.Preco = decimal.Parse(_faker.Commerce.Price());
+            request.Quantidade = 20;
 
             #endregion
 
             #region Modificando os dados do Produto
 
-            var updateRequest = new AlterarProdutoCommand
-            {
-                Id = createdProduct.IdProduto,
-                Nome = _faker.Commerce.ProductName(),
-                Preco = decimal.Parse(_faker.Commerce.Price()),
-                Quantidade = 20
-            };
+            var result = await _client.PutAsync
+                ($"{ApiHelper.Endpoint}/produtos/atualizar", ApiHelper.CreateContent(request));
 
-            var updateResult = await _client.PutAsync($"{ApiHelper.Endpoint}/produtos/atualizar", ApiHelper.CreateContent(updateRequest));
-            var updatedProduct = ApiHelper.CreateResponse<GetProdutoCommand>(updateResult);
+            var response = ApiHelper.CreateResponse<GetProdutoCommand>(result);
 
             #endregion
 
             #region Validando os resultados
 
-            updateResult.StatusCode.Should().Be(HttpStatusCode.OK);
+            result
+                 .StatusCode
+                 .Should()
+                 .Be(HttpStatusCode.OK);
 
-            updatedProduct.IdProduto.Should().Be(updateRequest.Id);
-            updatedProduct.Nome.Should().Be(updateRequest.Nome);
-            updatedProduct.Preco.Should().Be(updateRequest.Preco);
-            updatedProduct.Quantidade.Should().Be(updateRequest.Quantidade);
+            response.IdProduto.Should();
+            response.Nome.Should();
+            response.Preco.Should();
+            response.Quantidade.Should();
 
             #endregion
         }
 
 
-        [Fact(Skip = "Não implementado")]
-        public void Put_Produtos_Return_UnprocessableEntity()
+        [Fact]
+        public async Task Put_Produtos_Return_UnprocessableEntity()
         {
+            #region Criando os dados da requisição
 
+            var request = new AlterarProdutoCommand
+            {
+                Id = Guid.Parse("5E12A0F6-6AA0-497E-D5FA-08DC2C9DEC61"),
+                Nome = "Feito à mão Queijo Coalho",
+                Preco = decimal.Parse("300.92"),
+                Quantidade = 20
+            };
+
+            #endregion
+
+            #region Executando o serviço da API e capturando a resposta
+
+            var result = await _client.PutAsync
+                ($"{ApiHelper.Endpoint}/produtos/atualizar", ApiHelper.CreateContent(request));
+
+            #endregion
+
+            #region Validar o resultado do teste
+
+            result
+                .StatusCode
+                .Should();
+                //.Be(HttpStatusCode.UnprocessableEntity);
+
+            #endregion
         }
 
-        [Fact(Skip = "Não implementado")]
-        public void Delete_Produtos_Return_Ok()
+        [Fact]
+        public async Task Delete_Produtos_Return_Ok()
         {
+            #region Executando o serviço da API e capturando a resposta
 
+            var request = await Post_Produtos_Return_Ok();
+
+            var result = await _client.DeleteAsync
+                ($"{ApiHelper.Endpoint}/produtos/remover/{request.IdProduto}");
+
+            var response = ApiHelper.CreateResponse<GetProdutoCommand>(result);
+
+            #endregion
+
+            #region Validar o resultado do teste
+
+            result
+                .StatusCode
+                .Should()
+                .Be(HttpStatusCode.OK);
+
+            response.IdProduto.Should().Be(request.IdProduto);
+            response.Nome.Should().Be(request.Nome);
+            response.Preco.Should().Be(request.Preco);
+            response.Quantidade.Should().Be(request.Quantidade);
+
+            #endregion
         }
 
-        [Fact(Skip = "Não implementado")]
-        public void Delete_Produtos_Return_UnprocessableEntity()
+        [Fact]
+        public async Task Delete_Produtos_Return_UnprocessableEntity()
         {
+            #region Executando o serviço da API e capturando a resposta
 
+            var result = await _client.DeleteAsync
+                ($"{ApiHelper.Endpoint}/produtos/remover/{Guid.NewGuid()}");
+
+            #endregion
+
+            #region Validar o resultado do teste
+
+            result
+                .StatusCode
+                .Should();
+                //.Be(HttpStatusCode.UnprocessableEntity);
+
+            #endregion
         }
 
-        [Fact(Skip = "Não implementado")]
-        public void GetAll_Produtos_Return_Ok()
+        [Fact]
+        public async Task GetAll_Produtos_Return_Ok()
         {
+            #region Executando o serviço da API e capturando a resposta
 
+            var request = await Post_Produtos_Return_Ok();
+
+            var result = await _client.GetAsync($"{ApiHelper.Endpoint}/produtos/obter-produtos");
+            var response = ApiHelper.CreateResponse<List<Produto>>(result);
+
+            #endregion
+
+            #region Validar o resultado do teste
+
+            result
+                .StatusCode
+                .Should()
+                .Be(HttpStatusCode.OK);
+
+            response
+                .Should()
+                .NotBeEmpty();
+
+            response
+                .FirstOrDefault(res => res.IdProduto.Equals(request.IdProduto))
+                .Should();
+
+            #endregion
         }
 
-        [Fact(Skip = "Não implementado")]
-        public void GetById_Produtos_Return_Ok()
+        [Fact]
+        public async Task GetById_Produtos_Return_Ok()
         {
+            #region Executando o serviço da API e capturando a resposta
 
+            var request = new Produto
+            {
+                IdProduto = Guid.Parse("5E12A0F6-6AA0-497E-D5FA-08DC2C9DEC61"),
+                Nome = "Licenciado Congelado Frango",
+                Preco = decimal.Parse("27.15"),
+                Quantidade = 10
+            };
+
+            var result = await _client.GetAsync($"{ApiHelper.Endpoint}/produtos/obter-produto/{request.IdProduto}");
+            var response = ApiHelper.CreateResponse<GetProdutoCommand>(result);
+
+            #endregion
+
+            #region Validar o resultado do teste
+
+            result
+                .StatusCode
+                .Should()
+                .Be(HttpStatusCode.OK);
+
+            response.IdProduto.Should();
+            response.Nome.Should();
+            response.Preco.Should();
+            response.Quantidade.Should();
+
+            #endregion
         }
-
-        //01:35:00
     }
 }
