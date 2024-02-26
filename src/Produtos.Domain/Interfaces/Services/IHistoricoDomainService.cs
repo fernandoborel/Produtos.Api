156f6abfe32f2f9ebbe758cab9bf05f777130a68
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2024-02-26 15:07:18 -0300

    feat. tb_historico pt.1

---
+++ src/Produtos.Domain/Interfaces/Services/IHistoricoDomainService.cs
@@ -0,0 +1,13 @@
+﻿namespace Produtos.Domain.Interfaces.Services;
+
+public interface IHistoricoDomainService : IDisposable
+{
+    Task Adicionar(Historico historico);
+    Task Atualizar(Historico historico);
+    Task Remover(Guid id);
+
+    Task<IEnumerable<Historico>> ObterTodos();
+
+    Task<Historico> ObterPorId(Guid id);
+    Task<IEnumerable<Historico>> ObterPorProdutoId(Guid produtoId);
+}
---
+++ src/Produtos.Domain/Services/HistoricoDomainService.cs
@@ -0,0 +1,52 @@
+﻿using Produtos.Domain.Interfaces.Repositories;
+using Produtos.Domain.Interfaces.Services;
+
+namespace Produtos.Domain.Services;
+
+public class HistoricoDomainService : IHistoricoDomainService
+{
+    private readonly IHistoricoRepository _historicoRepository;
+
+    public HistoricoDomainService(IHistoricoRepository historicoRepository)
+    {
+        _historicoRepository = historicoRepository;
+    }
+
+    public async Task Adicionar(Historico historico)
+    {
+        await _historicoRepository.AddAsync(historico);
+    }
+
+    public async Task Atualizar(Historico historico)
+    {
+        await _historicoRepository.UpdateAsync(historico);
+    }
+
+    public async Task Remover(Guid id)
+    {
+        var produto = await _historicoRepository.GetAsync(id);
+        await _historicoRepository.DeleteAsync(produto);
+    }
+
+    public void Dispose()
+    {
+        _historicoRepository.Dispose();
+    }
+
+    public async Task<Historico> ObterPorId(Guid id)
+    {
+        var historico = await _historicoRepository.GetAsync(id);
+        return historico;
+    }
+
+    public async Task<IEnumerable<Historico>> ObterPorProdutoId(Guid produtoId)
+    {
+        var historico = await _historicoRepository.GetByProdutoId(produtoId);
+        return historico;
+    }
+
+    public async Task<IEnumerable<Historico>> ObterTodos()
+    {
+        return await _historicoRepository.GetAllAsync();
+    }
+}
